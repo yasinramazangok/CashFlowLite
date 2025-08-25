44273f263c7a01b5e34ae857b545e591799d1233
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-08-25 13:37:54 +0300

    feat(application): add service interfaces

---
+++ CashFlowLite/CashFlowLite.Application/Services/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using CashFlowLite.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlowLite.Application.Services
+{
+    public interface IAuthService
+    {
+        Task<bool> RegisterAsync(RegisterDto dto);
+        Task<string?> LoginAsync(LoginDto dto);
+    }
+}
---
+++ CashFlowLite/CashFlowLite.Application/Services/ITransactionService.cs
@@ -0,0 +1,14 @@
+﻿using CashFlowLite.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlowLite.Application.Services
+{
+    public interface ITransactionService
+    {
+        Task<IEnumerable<TransactionDto>> GetTransactionsByAccountAsync(int accountId);
+    }
+}
