e04bd6d45b3ad8588ff1146e5a4d0b633f566ff3
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-08-25 14:00:27 +0300

    feat(infrastructure): add repository implementations

---
+++ CashFlowLite/CashFlowLite.Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,50 @@
+﻿using CashFlowLite.Application.Repositories;
+using CashFlowLite.Domain.Entities;
+using CashFlowLite.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlowLite.Infrastructure.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly CashFlowLiteDbContext _context;
+
+        public AccountRepository(CashFlowLiteDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Account> GetByIdAsync(int id) =>
+            await _context.Accounts.Include(a => a.Transactions).FirstOrDefaultAsync(a => a.Id == id);
+
+        public async Task<IEnumerable<Account>> GetAllAsync() =>
+            await _context.Accounts.ToListAsync();
+
+        public async Task AddAsync(Account account)
+        {
+            await _context.Accounts.AddAsync(account);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Account account)
+        {
+            _context.Accounts.Update(account);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+            if (account != null)
+            {
+                _context.Accounts.Remove(account);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
