8716aa5bc75e2aee83bd48a74af069b9abea5b62
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-08-25 13:42:22 +0300

    feat(application): add repository interfaces

---
+++ CashFlowLite/CashFlowLite.Application/Repositories/IAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using CashFlowLite.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlowLite.Application.Repositories
+{
+    public interface IAccountRepository
+    {
+        Task<Account> GetByIdAsync(int id);
+        Task<IEnumerable<Account>> GetAllAsync();
+        Task AddAsync(Account account);
+        Task UpdateAsync(Account account);
+        Task DeleteAsync(int id);
+    }
+}
