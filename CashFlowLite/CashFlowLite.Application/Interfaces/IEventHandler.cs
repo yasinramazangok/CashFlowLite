56dcb0a8b11beddd51d97598fadf6386b626604a
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-08-26 03:49:24 +0300

    feat(solution): add domain event propagation for deposit-withdraw operations

---
+++ CashFlowLite/CashFlowLite.Application/Interfaces/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using CashFlowLite.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlowLite.Application.Interfaces
+{
+    public interface IEventHandler<TEvent> where TEvent : IDomainEvent
+    {
+        Task Handle(TEvent domainEvent);
+    }
+}
---
+++ CashFlowLite/CashFlowLite.Domain/Concretes/DepositMadeEvent.cs
@@ -0,0 +1,21 @@
+﻿using CashFlowLite.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlowLite.Domain.Concretes
+{
+    public class DepositMadeEvent : IDomainEvent
+    {
+        public int AccountId { get; }
+        public decimal Amount { get; }
+
+        public DepositMadeEvent(int accountId, decimal amount)
+        {
+            AccountId = accountId;
+            Amount = amount;
+        }
+    }
+}
